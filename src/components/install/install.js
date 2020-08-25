b007da8bd35e7f09525ce47ae26c8748efa0732a
Samson <samsonudo@yahoo.com>
2020-08-25 15:47:21 +0100

    Electron

---
+++ src/components/install/install.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Install = () => {
+  return (
+    <div>
+      Hi
+    </div>
+  )
+}
+
+export default Install;
--- src/components/loading/loading.js
+++ src/components/loading/loading.js
@@ -3,12 +3,43 @@ import './loading.scss';
 
 const Loading = () => {
   return (
-    <div className="container">
-     <h1 classNameName="text-center">Samson</h1>
-     <div className="progress">
-       <div className="progress-bar"></div>
+    <body>
+      <div className="loader">
+        <div className="box box0">
+          <div></div>
+        </div>
+        <div className="box box1">
+          <div></div>
+        </div>
+        <div className="box box2">
+          <div></div>
+        </div>
+        <div className="box box3">
+          <div></div>
+        </div>
+        <div className="box box4">
+          <div></div>
+        </div>
+        <div className="box box5">
+          <div></div>
+        </div>
+        <div className="box box6">
+          <div></div>
+        </div>
+        <div className="box box7">
+          <div></div>
+        </div>
+        <div className="ground">
+          <div></div>
+        </div>
+      </div>
+      <div className="container">
+       <h1 className="text-center">Loading</h1>
      </div>
-   </div>
+     <div className="dribbble">
+      <h1 className="text-center">Samson</h1>
+     </div>
+    </body>
   )
 }
 
